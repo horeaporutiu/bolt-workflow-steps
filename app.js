2abcb77a864818699b8920dbe600432c68369a10
Glitch (hello-express) <none>
2021-04-12 05:33:34 +0000

    🌯👷 Checkpoint ./app.js:30820119/4042

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Require the Bolt package (github.com/slackapi/bolt)
-const { App } = require("@slack/bolt");
+const { App, WorkflowStep } = require('@slack/bolt');
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
@@ -7,13 +7,40 @@ const app = new App({
 });
 
 
+
+
   console.log('before app . event')
 
 
 // All the room in the world for your code
 app.event('workflow_step_execute', async ({ event, client, context }) => {
+  
+
   console.log('workflow step has been executed')
   try {
+    
+      // Create a new WorkflowStep instance
+    const ws = new WorkflowStep('ticketData', {
+    edit: async ({ ack, step, configure }) => {},
+    save: async ({ ack, step, update }) => {},
+    execute: async ({ step, complete, fail }) => {
+       const { inputs } = step;
+    
+      console.log('inputs)
+      console.log()
+    const outputs = {
+      taskName: inputs.taskName.value,
+      taskDescription: inputs.taskDescription.value,
+    };
+
+    // if everything was successful
+    await complete({ outputs });
+
+    },
+  });
+
+  app.step(ws);
+    
     console.log('logging the event: ')
     console.log(event)
     
