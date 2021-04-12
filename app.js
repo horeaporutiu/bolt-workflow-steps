82f637a7eacd77fa9b1118139700ca67be9f382c
Glitch (hello-express) <none>
2021-04-12 05:57:38 +0000

    🎐☃️ Checkpoint ./app.js:30820119/6172

--- app.js
+++ app.js
@@ -15,42 +15,103 @@ const app = new App({
 // All the room in the world for your code
 app.event('workflow_step_execute', async ({ event, client, context }) => {
   
-
+  
   console.log('workflow step has been executed')
   try {
     
       // Create a new WorkflowStep instance
     const ws = new WorkflowStep('ticketData', {
-    edit: async ({ ack, step, configure }) => {},
     
-    save: async ({ ack, step, update }) => {
-      
-    },
-    
-    //when a user executes the step
-    execute: async ({ step, complete, fail }) => {
-       const { inputs } = step;
-    
-      console.log('inputs')
-      console.log(inputs)
-    const outputs = {
-      taskName: inputs.taskName.value,
-      taskDescription: inputs.taskDescription.value,
-    };
+      edit: async ({ ack, step, configure }) => {
+        await ack();
+        const blocks = [
+          {
+            type: 'input',
+            block_id: 'task_name_input',
+            element: {
+              type: 'plain_text_input',
+              action_id: 'name',
+              placeholder: {
+                type: 'plain_text',
+                text: 'Add a task name',
+              },
+            },
+            label: {
+              type: 'plain_text',
+              text: 'Task name',
+            },
+          },
+          {
+            type: 'input',
+            block_id: 'task_description_input',
+            element: {
+              type: 'plain_text_input',
+              action_id: 'description',
+              placeholder: {
+                type: 'plain_text',
+                text: 'Add a task description',
+              },
+            },
+            label: {
+              type: 'plain_text',
+              text: 'Task description',
+            },
+          },
+        ];
 
-    // if everything was successful
-    await complete({ outputs });
+    await configure({ blocks });
+      },
 
-    },
-  });
+  save: async ({ ack, step, view, update }) => {
+        await ack();
+        const { values } = view.state;
+        const taskName = values.task_name_input.name;
+        const taskDescription = values.task_description_input.description;
 
-  app.step(ws);
-    
-    console.log('logging the event: ')
-    console.log(event)
-    
-  }
-  catch (error) {
+        const inputs = {
+          taskName: { value: taskName.value },
+          taskDescription: { value: taskDescription.value }
+        };
+
+        const outputs = [
+          {
+            type: 'text',
+            name: 'taskName',
+            label: 'Task name',
+          },
+          {
+            type: 'text',
+            name: 'taskDescription',
+            label: 'Task description',
+          }
+        ];
+
+        await update({ inputs, outputs });
+      },
+
+      //when a user executes the step
+      execute: async ({ step, complete, fail }) => {
+         const { inputs } = step;
+
+        console.log('inputs')
+        console.log(inputs)
+      const outputs = {
+        taskName: inputs.taskName.value,
+        taskDescription: inputs.taskDescription.value,
+      };
+
+      // if everything was successful
+      await complete({ outputs });
+
+      },
+    });
+
+    app.step(ws);
+
+      console.log('logging the event: ')
+      console.log(event)
+
+  } catch (error) {
     console.error(error);
   }
 });
