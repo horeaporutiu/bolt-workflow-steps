761db16d8268dbad0f6b79066c8ce55bbd4ab235
Glitch (hello-express) <none>
2021-04-12 04:22:38 +0000

    🐌🌿 Checkpoint ./app.js:30820119/1738

--- app.js
+++ app.js
@@ -7,55 +7,16 @@ const app = new App({
 });
 
 
+  console.log('before app . event')
+
 
 // All the room in the world for your code
-app.event('message.app_home', async ({ event, client, context }) => {
+app.event('workflow_step_execute', async ({ event, client, context }) => {
+  console.log('workflow step has been executed')
   try {
-    /* view.publish is the method that your app uses to push a view to the Home tab */
-    const result = await client.views.publish({
-
-      /* the user that opened your app's app home */
-      user_id: event.user,
-
-      /* the view object that appears in the app home*/
-      view: {
-        type: 'home',
-        callback_id: 'home_view',
-
-        /* body of the view */
-        blocks: [
-          {
-            "type": "section",
-            "text": {
-              "type": "mrkdwn",
-              "text": "*Welcome to your _App's Home_* :tada:"
-            }
-          },
-          {
-            "type": "divider"
-          },
-          {
-            "type": "section",
-            "text": {
-              "type": "mrkdwn",
-              "text": "This button won't do much for now but you can set up a listener for it using the `actions()` method and passing its unique `action_id`. See an example in the `examples` folder within your Bolt app."
-            }
-          },
-          {
-            "type": "actions",
-            "elements": [
-              {
-                "type": "button",
-                "text": {
-                  "type": "plain_text",
-                  "text": "Click me!"
-                }
-              }
-            ]
-          }
-        ]
-      }
-    });
+    console.log('logging the event: ')
+    console.log(event)
+    
   }
   catch (error) {
     console.error(error);
@@ -67,5 +28,5 @@ app.event('message.app_home', async ({ event, client, context }) => {
   // Start your app
   await app.start(process.env.PORT || 3000);
 
-  console.log('⚡️ Bolt app is running!');
+  console.log('⚡️ Bolt app is running! HSFHDS');
 })();
